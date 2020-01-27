666fce2c7778a7396f5f4a1e0b3b5fc6e6d4b453
Nidal HADDAD <haddad.nidal1@gmail.com>
2020-01-27 18:33:27 +0100

    volume option to jadeed.sh

--- scripts/jadeed.sh
+++ scripts/jadeed.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #Maintainer nidal haddad
 #----Launch aljadeed on mpv
-`mpv http://livecdnh3.tvanywhere.ae/hls/jadeed/02.m3u8 &`
+cmd="mpv http://livecdnh3.tvanywhere.ae/hls/jadeed/02.m3u8 --volume 35"
+$cmd &
