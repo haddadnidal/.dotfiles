6754a74614ab938e978b375b252a71de4dd68b3c
Nidal HADDAD <haddad.nidal1@gmail.com>
2020-01-28 11:11:34 +0100

    add nohup to jadded.sh and create ovt.sh

--- scripts/jadeed.sh
+++ scripts/jadeed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #Maintainer nidal haddad
 #----Launch aljadeed on mpv
-cmd="mpv http://livecdnh3.tvanywhere.ae/hls/jadeed/02.m3u8 --volume 35"
+cmd=" nohup mpv http://livecdnh3.tvanywhere.ae/hls/jadeed/02.m3u8 --volume 35"
 $cmd &
---
+++ scripts/otv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Maintainer nidal haddad
+#----Short description of the file
+cmd=" nohup mpv https://svs.itworkscdn.net/otvlebanonlive/otv.smil/playlist_406p.m3u8 --volume 35"
+$cmd &
